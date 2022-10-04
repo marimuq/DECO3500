f962773e76b8911e592eb573991ce501b411db70
Chaashya <fernandochaashya@gmail.com>
2022-10-04 16:48:51 +1000

    added timeline user legend

--- quokka_app/lib/main.dart
+++ quokka_app/lib/main.dart
@@ -92,6 +92,16 @@ class TripBoard extends StatelessWidget {
                 );
               },
             ),
+            ElevatedButton(
+              child: Text('Timeline'),
+              onPressed: () {
+                Text('pressed');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Timeline()),
+                );
+              },
+            )
           ],
         ));
   }
@@ -121,6 +131,62 @@ class Activities extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [Text('Gallery of Modern Arts')],
                 ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Timeline extends StatelessWidget {
+  const Timeline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timeline'),
+      ),
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      WidgetSpan(
+                        child: Icon(Icons.circle, size: 14, color: Colors.pink),
+                      ),
+                      TextSpan(
+                        text: " User 1\n",
+                      ),
+                      WidgetSpan(
+                        child: Icon(Icons.square, size: 14, color: Colors.blue),
+                      ),
+                      TextSpan(
+                        text: " User 1\n",
+                      ),
+                      WidgetSpan(
+                        child: Icon(Icons.star, size: 14, color: Colors.green),
+                      ),
+                      TextSpan(
+                        text: " User 3\n",
+                      ),
+                      WidgetSpan(
+                        child: Icon(Icons.eco, size: 14, color: Colors.purple),
+                      ),
+                      TextSpan(
+                        text: " User 4\n",
+                      ),
+                    ],
+                  ),
+                )
               ],
             )
           ],
