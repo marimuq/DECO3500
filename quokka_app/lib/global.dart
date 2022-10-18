0c0a610df357df899f0886818ab71083ec851604
masofiav <ma.sofiavaldez@y7mail.com>
2022-10-18 15:45:47 +1000

    activities, rough layout, timeline model

---
+++ quokka_app/lib/global.dart
@@ -0,0 +1,7 @@
+// Activities voted by user
+int royal_botanic_voted = -1;
+int eureka_skydeck_voted = -1;
+int old_melbourne_voted = -1;
+int queen_victoria_voted = -1;
+int shrine_remembrance_voted = -1;
+int melbourne_zoo_voted = -1;
--- quokka_app/lib/main.dart
+++ quokka_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+int royalBotanicVoted = -1;
+
 void main() {
   runApp(const Quokka());
 }
@@ -21,14 +23,14 @@ class _QuokkaState extends State<Quokka> {
       home: Builder(
         builder: (context) => Scaffold(
           appBar: AppBar(
-            title: Text('Quokka'),
+            title: const Text('Quokka'),
           ),
           drawer: Drawer(
             child: ListView(
               padding: EdgeInsets.zero,
               children: <Widget>[
                 ListTile(
-                  title: Text('My Profile'),
+                  title: const Text('My Profile'),
                   onTap: () {
                     setState(() {
                       mainWidget = Profile();
@@ -37,7 +39,7 @@ class _QuokkaState extends State<Quokka> {
                   },
                 ),
                 ListTile(
-                  title: Text('Trip Board'),
+                  title: const Text('Trip Board'),
                   onTap: () {
                     setState(() {
                       mainWidget = TripBoard();
@@ -59,7 +61,99 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('My Profile'),
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: Column(children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Buddy QLD Trip Board', textAlign: TextAlign.left),
+                Icon(Icons.dehaze)
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InkWell(
+                    child: Container(
+                        width: 200,
+                        height: 200,
+                        color: Colors.blueGrey,
+                        child: ElevatedButton(
+                          child: Text('Destinations'),
+                          onPressed: () {
+                            null;
+                          },
+                        ))),
+                const SizedBox(width: 10),
+                InkWell(
+                    child: Container(
+                        width: 200,
+                        height: 200,
+                        color: Colors.blueGrey,
+                        child: ElevatedButton(
+                          child: Text('Dates'),
+                          onPressed: () {
+                            null;
+                          },
+                        ))),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InkWell(
+                    child: Container(
+                        width: 200,
+                        height: 200,
+                        color: Colors.blueGrey,
+                        child: ElevatedButton(
+                          child: Text('Flights'),
+                          onPressed: () {
+                            null;
+                          },
+                        ))),
+                const SizedBox(width: 10),
+                InkWell(
+                    child: Container(
+                        width: 200,
+                        height: 200,
+                        color: Colors.blueGrey,
+                        child: ElevatedButton(
+                          child: Text('Accommodations'),
+                          onPressed: () {
+                            null;
+                          },
+                        ))),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Daily Planner',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              child: Container(
+                width: 400,
+                height: 100,
+                child: ElevatedButton(
+                  child: const Text('Activities'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Activities()),
+                    );
+                  },
+                ),
+              ),
+            )
+          ])),
     );
   }
 }
@@ -69,7 +163,7 @@ class TripBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             'Trip Board',
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18, color: Colors.black45),
@@ -83,15 +177,58 @@ class TripBoard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             ElevatedButton(
-              child: Text('Activities'),
+              child: const Text('Activities'),
               onPressed: () {
-                Text('pressed');
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const Activities()),
                 );
               },
             ),
+          ],
+        ));
+  }
+}
+
+class Timeline extends StatelessWidget {
+  const Timeline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Timeline'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.list),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Activities()),
+              );
+            }),
+        body: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 50.0),
+              child: Card(
+                margin: EdgeInsets.all(20),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            Positioned(
+                top: 0,
+                bottom: 0,
+                left: 35,
+                child: Container(
+                  height: double.infinity,
+                  width: 5,
+                  color: Colors.blue,
+                ))
           ],
         ));
   }
@@ -106,26 +243,277 @@ class Activities extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Activities'),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.timeline),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Timeline()),
+            );
+          }),
       body: Center(
         child: ListView(
           padding: const EdgeInsets.all(8),
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                    'assets/melbourne/royal_botanic_gardens_victoria.jpg',
+                    width: 100,
+                    fit: BoxFit.fill),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      child: const Text(
+                          'Royal Botanic Gardens Victoria\nDuration: 1-2 hours'),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          onPressed: () {
+                            _displayDialog(context, "royal_botanic_voted", 1);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.thumb_down),
+                          onPressed: () {
+                            _displayDialog(context, "royal_botanic_voted", 0);
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Image.asset('assets/brisbane/gallery_of_modern_arts.jpg',
+                Image.asset('assets/melbourne/eureka_skydeck.jpg',
                     width: 100, fit: BoxFit.fill),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [Text('Gallery of Modern Arts')],
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      child: const Text('Eureka Skydeck\nDuration: 1-2 hours'),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.thumb_down),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        )
+                      ],
+                    )
+                  ],
                 ),
               ],
-            )
+            ),
+            const SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/melbourne/old_melbourne_gaol.jpg',
+                    width: 100, fit: BoxFit.fill),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      child:
+                          const Text('Old Melbourne Gaol\nDuration: 1-2 hours'),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.thumb_down),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/melbourne/queen_victoria_market.jpg',
+                    width: 100, fit: BoxFit.fill),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      child: const Text(
+                          'Queen Victoria Market\nDuration: 2-3 hours'),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.thumb_down),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/melbourne/shrine_of_remembrance.jpg',
+                    width: 100, fit: BoxFit.fill),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      child: const Text(
+                          'Shrine of Remembrance\nDuration: 1-2 hours'),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.thumb_down),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/melbourne/melbourne_zoo.jpg',
+                    width: 100, fit: BoxFit.fill),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      child: const Text('Melbourne Zoo\nDuration: 2-3 hours'),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.thumb_down),
+                          color: Colors.grey,
+                          onPressed: () {
+                            null;
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+
+  _displayDialog(BuildContext context, String activity, int vote) async {
+    switch (activity) {
+      case "royal_botanic_voted":
+        royalBotanicVoted = vote;
+        print("vote variable changed");
+        break;
+    }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Expanded(
+              child: AlertDialog(
+                  content: const Text('Activity added to timeline'),
+                  actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Ok'),
+                )
+              ]));
+        });
   }
 }
